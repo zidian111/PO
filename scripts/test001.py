543e8f810de58aa1b6349f7c38c0d7677f4d76e2
1342710330 <1342710330@qq.com>
2018-05-27 16:22:48 +0800

    更新

--- scripts/test001.py
+++ scripts/test001.py
@@ -1,5 +1,11 @@
-import  pytest
+import  pytest,allure
 class  Test_001:
+    @allure.step(title="测试步骤001")
     @pytest.mark.parametrize("a",[1,2,3])
     def test_abc(self,a):
         assert  a !=2
+
+    @allure.step(title="测试步骤002")
+    @pytest.mark.parametrize("b", [1, 2, 3])
+    def test_abc(self, b):
+        assert  b != 2
